df672cb90cd94e1808e8ca2b7d5d1a19183451fe
Leonardorossato <leonardorossato21@gmail.com>
2022-12-10 14:32:32 -0300

    modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), CacheModule.register()],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
