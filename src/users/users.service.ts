705ac77d160cc1fdbb1f926d6b9e0a140a56b43a
Leonardorossato <leonardorossato21@gmail.com>
2022-12-10 14:32:42 -0300

    all services ready

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,16 +5,24 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { randomBytes } from 'crypto';
 import { FindOneOptions, Repository } from 'typeorm';
+import * as cacheManager from 'cache-manager';
 import { CreateUserDTO } from './dto/create.user.dto';
 import { Users } from './entities/user.entity';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UsersService {
+  public memoryCache;
+
   constructor(
     @InjectRepository(Users)
     private readonly usersRepository: Repository<Users>,
-  ) {}
+    private readonly mailService: MailerService,
+  ) {
+    this.memoryCache = cacheManager.caching('memory');
+  }
 
   async all(): Promise<Users[]> {
     try {
@@ -35,12 +43,45 @@ export class UsersService {
     return user;
   }
 
+  async findByEmail(email: string) {
+    try {
+      const user = await this.usersRepository.findOneBy({ email: email });
+      if (user) {
+        throw new HttpException('Email not found', HttpStatus.NOT_FOUND);
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException(
+        'Erro to fin a userEmail',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
   async create(dto: CreateUserDTO) {
     try {
-      const user = await this.usersRepository.save({
-        ...dto,
+      const email = dto.email;
+      const recoverToken = randomBytes(3).toString('hex');
+      await this.memoryCache.set(recoverToken, { ...dto });
+      const userEmail = await this.usersRepository.findOneBy({
+        email: dto.email,
       });
-      return user;
+      if (userEmail) {
+        throw new HttpException('Email already exitis', HttpStatus.BAD_REQUEST);
+      }
+      if (!userEmail) {
+        const mail = {
+          to: email,
+          from: 'noreply@application.com',
+          subject: 'Confirmação do email',
+          template: 'confirmation',
+          context: {
+            token: recoverToken,
+          },
+        };
+        await this.mailService.sendMail(mail);
+      }
+      return await this.usersRepository.save(userEmail);
     } catch (error) {
       throw new NotFoundException('Erro to create a new User');
     }
