827177264a35e37aadd95821b49806af4f7273f7
Leonardorossato <leonardorossato21@gmail.com>
2022-12-10 10:46:11 -0300

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Ecommere Backned')
+    .setDescription(
+      'The ecommerce API description using: Nestjs, Docker, Postgres and Swagger',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: true,
+  });
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
