09218f471f8ff1a7740c32e27cbb8eceec3cbb88
Leonardorossato <leonardorossato21@gmail.com>
2022-12-10 11:20:34 -0300

    user controller ok

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 
+@ApiTags('Usuarios')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/all')
+  async getAllUsers() {
+    return await this.usersService.all();
+  }
+
+  @Delete('/delete/:id')
+  async deletedUserById(@Body() id: number) {
+    return await this.usersService.delete(id);
+  }
 }
