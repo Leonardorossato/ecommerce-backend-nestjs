667852d2e2774a3c6f75a9e8d664e4c9f8fa40fa
Leonardorossato <leonardorossato21@gmail.com>
2022-12-14 20:47:07 -0300

    clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { StripeModule } from 'nestjs-stripe';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmAsyncConfig } from './config/ormconfig';
 import { UsersModule } from './users/users.module';
-
 
 @Module({
   imports: [
@@ -13,10 +11,6 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     ConfigModule.forRoot({ isGlobal: true }),
-    StripeModule.forRoot({
-      apiKey: process.env.STRIPE_PUBLIC_KEY,
-      apiVersion: '2022-11-15',
-    }),
   ],
 })
 export class AppModule {}
